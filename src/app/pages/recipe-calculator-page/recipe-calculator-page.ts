665fe327a556c1638386d091d81683dd3f3453c4
KLNPRO <killianemmanuel68@gmail.com>
2026-03-20 16:17:08 +0100

    1er commit

---
+++ src/app/pages/recipe-calculator-page/recipe-calculator-page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-recipe-calculator-page',
+  imports: [],
+  templateUrl: './recipe-calculator-page.html',
+  styleUrl: './recipe-calculator-page.css',
+})
+export class RecipeCalculatorPage {
+
+}
