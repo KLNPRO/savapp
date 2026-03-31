0f7b3b1036405a7ac0fbc9cb4500de869831d4a2
KLNPRO <killianemmanuel68@gmail.com>
2026-03-31 10:22:16 +0200

    troisieme

--- src/app/interceptors/auth-interceptor.ts
+++ src/app/interceptors/auth-interceptor.ts
@@ -6,8 +6,7 @@ const authService = inject(AuthService);
 const token = authService.getToken();
 // Clonnage de la requête pour lui ajouter le token JWT :
 if (token) {
-const cloneReq = req.clone({setHeaders: { Authorization: `Bearer
-${token}`}});
+const cloneReq = req.clone({setHeaders: { Authorization: `Bearer ${token}`}});
 return next(cloneReq); // Requête contenant le jeton
 }
 return next(req); // La requête passe à travers.
